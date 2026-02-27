234ceacfa654e1b2bde7f5ff7c4f1a45e9f97473
Juan Parra <hola.socio.it@gmail.com>
2026-02-27 00:37:29 -0500

    segunda entrega

--- script.js
+++ script.js
@@ -1,93 +1,116 @@
-var champions = [{"name": "Ashe", "line": "ADC", "type": "Marksman"},
-                 {"name": "Leona", "line": "Support", "type": "Tank"},
-                 {"name": "Ahri", "line": "Mid", "type": "Mage"},
-                 {"name": "Garen", "line": "Top", "type": "Fighter"},
-                 {"name": "Lee Sin", "line": "Jungle", "type": "Assassin"},
-                 {"name": "Soraka", "line": "Support", "type": "Support"},
-                 {"name": "Caitlyn", "line": "ADC", "type": "Marksman"},
-                 {"name": "Lux", "line": "Mid", "type": "Mage"},
-                 {"name": "Thresh", "line": "Support", "type": "Tank"},
-                 {"name": "Darius", "line": "Top", "type": "Fighter"},
-                 {"name": "Zed", "line": "Mid", "type": "Assassin"},
-                 {"name": "Teemo", "line": "Top", "type": "Marksman"},
-                 {"name": "Jinx", "line": "ADC", "type": "Marksman"},
-                 {"name": "Katarina", "line": "Mid", "type": "Assassin"},
-                 {"name": "Blitzcrank", "line": "Support", "type": "Tank"},
-                 {"name": "Yasuo", "line": "Mid", "type": "Fighter"},
-                 {"name": "Riven", "line": "Top", "type": "Fighter"},
-                 {"name": "Graves", "line": "ADC", "type": "Marksman"},
-                 {"name": "Evelynn", "line": "Jungle", "type": "Assassin"},
-                 {"name": "Syndra", "line": "Mid", "type": "Mage"},
-                 {"name": "Ekko", "line": "Mid", "type": "Assassin"}];
+import { getChampions, getRoles , getChampion } from "./service.js";
 
 
-var championLine = ["ADC", "Support", "Mid", "Top", "Jungle"];
-var championType = ["Marksman", "Mage", "Tank", "Assassin", "Fighter", "Support"];
 
-let championName = '';
+console.log("entera",getChampions())
+console.log("filtrada",getChampions("Marksman"))
+console.log("champ",getChampion("Akshan"))
+console.log("entera",getRoles())
+console.log("filtrada",getRoles("Akshan"))
 
 
-function selectChampionLine() {
-    var select = prompt("Select your champion line: " + championLine.join(", "));
-    if (championLine.includes(select)) {
-        alert("✓ You have selected the " + select + " line.");
-        return select;
-    } else {
-        alert("✗ Invalid selection. Please try again.");
-        return selectChampionLine();
+var champions = await getChampions();
+var roles = await getRoles();
+
+let championsVsName = {
+                        'rol1':{
+                            'nameRol':'',
+                            'champions': [],
+                            'champion': {
+                                'name': '',
+                                'values':{}
+                            }
+
+                        },
+                        'rol2':{
+                            'nameRol':'',
+                            'champions': [],
+                            'champion': {
+                                'name': '',
+                                'values':{}
+                            }
+                        }
+                        };
+
+function renderSelectRol(id){
+    const select = document.getElementById(id)
+
+    select.innerHTML = `<option value="" selected disabled hidden>Selecciona un rol</option>
+                        ${roles.map(rol => `<option value="${rol}">${rol}</option>`)}`
+    select.onchange = async () => await setValueRol(id);
+}
+
+async function setValueRol(id){
+    const select = document.getElementById(id)
+
+    const rol = championsVsName[id]
+    rol.nameRol = select.value
+
+    rol.champions = await getChampions(rol.nameRol)
+    if(id === "rol1") {
+        renderSelectChampion("cham1", "rol1")
+    }else{
+        renderSelectChampion("cham2", "rol2")
     }
 }
 
-function selectChampionType() {
-    var select = prompt("Select your champion type: " + championType.join(", "));
-    if (championType.includes(select)) {
-        alert("✓ You have selected the " + select + " type.");
-        return select;
-    } else {
-        alert("✗ Invalid selection. Please try again.");
-        return selectChampionType();
-    }
+function renderSelectChampion(id, rol){
+    const select = document.getElementById(id)
+    const listCham = championsVsName[rol]?.champions ;
+    select.innerHTML = `<option value="" selected disabled hidden>Selecciona un champ</option>
+    ${listCham.map(champ => `<option value="${champ.id}">${champ.id}</option>`)}`
+
+    
+    select.onchange = async () => await setValueChampion(id, rol);
+}
+
+async function setValueChampion(id, ro){
+    const select = document.getElementById(id)
+
+    const rol = championsVsName[ro]
+    rol.champion.name = select.value
+
+    rol.champion.values = await getChampion(rol.champion.name)
+    renderCard(id, ro)
+}
+
+function renderCard(id, rolKey) {
+    const container = document.getElementById(id + rolKey);
+    console.log(id + rolKey)
+    if (!container) return;
+    console.log("aquì1")
+    const championData = championsVsName[rolKey].champion.values;
+    const data = Object.values(championData)[0];
+
+    if (!data) return;
+
+    container.innerHTML = `
+        <div class="card">
+            <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${data.id}_0.jpg" 
+                 alt="${data.name}" style="width:100%; border-radius: 8px;">
+            
+            <h2>${data.name}</h2>
+            <p><i>${data.title}</i></p>
+            
+            <div class="stats">
+                <p>❤️ <b>Vida:</b> ${data.stats.hp}</p>
+                <p>🛡️ <b>Armadura:</b> ${data.stats.armor}</p>
+                <p>🌀 <b>Dificultad:</b> ${data.info.difficulty}/10</p>
+            </div>
+
+            <div class="lore">
+                <p>${data.blurb}</p>
+            </div>
+
+            <div class="tags">
+                ${data.tags.map(tag => `<span class="badge">${tag}</span>`).join('')}
+            </div>
+        </div>
+    `;
 }
 
 
-function selectChampion(selectLine, selectType) {
-    let posibleChampions = [];
-    for (let i = 0; i < champions.length; i++) {
-        if(champions[i].line === selectLine && champions[i].type === selectType) {
-            posibleChampions.push(champions[i].name);
-        }
-    }
-    if (posibleChampions.length > 0) {
-        alert("Available champions for " + selectLine + " (" + selectType + "): \n" + posibleChampions.join(", "));
-        var select = prompt("Select your champion from the list above: "+ posibleChampions.join(", "));
-        if (posibleChampions.includes(select)) {
-            alert("✓ You have selected " + select + " as your champion.");
-            return select;
-        } else {
-            alert("✗ Invalid champion selection. Please try again.");
-            return selectChampion(selectLine, selectType);
-        }
-    } else {
-        alert("✗ No champions found for " + selectLine + " (" + selectType + "). Try again.");
-        return '';
-    }
-}
-
-function startSelector() {
-    let selectLine = selectChampionLine();
-    let selectType = selectChampionType();
-    let selectedChampion = selectChampion(selectLine, selectType);
-    if (selectedChampion != '') {
-        alert("Champion selection completed: " + selectedChampion);
-        return selectedChampion;
-    } else {
-        alert("No champion selected. Starting over...");
-        return startSelector();
-    }
-}
-
-
-
-do {
-    startSelector();
-}while (championName != '' );
+renderSelectRol("rol1")
+renderSelectRol("rol2")
+renderSelectChampion("cham1", "rol1")
+renderSelectChampion("cham2", "rol2")
---
+++ service.js
@@ -0,0 +1,82 @@
+const path_api = "https://ddragon.leagueoflegends.com";
+// const quetrae las versiones de la API de personajes de League of Legends siendo la priimera la ultima version
+ async function getVersion() {
+    try {
+        const response = await fetch(path_api + "/api/versions.json");
+        const data = await response.json();
+        return data[0];
+    } catch (error) {
+        console.error("Error de tipo: ", error);
+        return null;
+    }
+}
+
+
+
+//Funtion para traer la lista de campeones de la version mas reciente
+async function getChampions(rol) {
+    try {
+        const version = await getVersion()
+        const url = `https://ddragon.leagueoflegends.com/cdn/${version}/data/es_ES/champion.json`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if(!rol){
+            return Object.values(data.data);
+
+        }else{
+            const champions = Object.values(data.data).filter(champion => {
+                const roles =champion.tags;
+                return (roles.includes(rol))
+            });
+            return champions;
+        }
+    } catch (error) {
+        console.error("Error de tipo: ", error);
+        return null;
+}
+}
+
+//Funtion para traer el campeon por el nombre de la version mas reciente
+async function getChampion(name){
+    try {
+        const version = await getVersion();
+        const url = `https://ddragon.leagueoflegends.com/cdn/${version}/data/es_ES/champion/${name}.json`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        return data.data;
+    } catch (error) {
+        console.error("Error de tipo: ", error);
+        return null;
+    }
+}
+
+//Funtion para traer la lista de roles todos o filtrado por campeon
+async function getRoles(name){
+    let roles = [];
+    let championsJson;
+    let champions = [];
+    try {
+        if(name){
+            championsJson = await getChampion(name);
+            champions.push(championsJson.data);
+        }else{
+            championsJson = await getChampions();
+        }
+        champions = Object.values(championsJson)
+        for(const champion of  champions){
+            const chamRoles = champion.tags;
+             for (const rol of chamRoles) {
+                if( !roles.includes(rol)){
+                    roles.push(rol)
+                }
+            }
+        }
+        return roles;
+    }catch (error){
+        console.error("Error de tipo: ", error);
+    }
+}
+
+
+export {getChampions, getChampion, getVersion, getRoles}
